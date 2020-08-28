dc7d8f9c9750814322d8a05ed3cf76e9ffe56c33
matt22881 <47930310+matt22881@users.noreply.github.com>
2020-08-28 15:31:37 -0400

    complete..ish

--- index.js
+++ index.js
@@ -109,72 +109,99 @@ const cat = {
 
 
 /*
-TASK 4 🚀
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+// TASK 4 🚀
+// /*
+//   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+//   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
+//   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
 
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
+//   Each constructor function has unique properties and methods that are defined in their block comments below:
+// */
 
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
+// /*
+//   === GameObject ===
+//   * createdAt
+//   * name
+//   * dimensions (These represent the character's size in the video game)
+//   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+// */
 
-function GameObject(createdAt, name, dimensions) {
-  this.createdAt = createdAt;
-  this.name = name;
-  this.dimensions = dimensions;
-}
-GameObject.prototype.destroy = function() {
-  return `${this.name} was removed from the game.`
-}
+// function GameObject(createdAt, name, dimensions) {
+//   this.createdAt = createdAt;
+//   this.name = name;
+//   this.dimensions = dimensions;
+// }
+// GameObject.prototype.destroy = function() {
+//   return `${this.name} was removed from the game.`
+// }
 
+// const fourSidedDie = new GameObject("Friday", "Four Sided Die", {length: 1, width: 1, height: 1})
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+// /*
+//   === CharacterStats ===
+//   * healthPoints
+//   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+//   * should inherit destroy() from GameObject's prototype
+// */
 
-function CharacterStats(healthPoints) {
-  GameObject.call (this, createdAt, name, dimensions);
-  this.healthPoints = healthPoints
-};
-CharacterStats.prototype = Object.create(GameObject.prototype);
-CharacterStats.prototype.takeDamage = function() {
-  return (`${name} took damage.`)
-};
+// function CharacterStats(createdAt, name, dimensions, healthPoints) {
+//   GameObject.call (this, healthPoints);
+//   this.createdAt = createdAt;
+//   this.name = name;
+//   this.dimensions = dimensions;
+//   this.healthPoints = healthPoints;
+// };
+// CharacterStats.prototype = Object.create(GameObject.prototype);
+// CharacterStats.prototype.takeDamage = function() {
+//   return (`${name} took damage.`)
+// };
 
+// const oldSage = new CharacterStats("Today", "Old Sage", {length: 1, width: 2, height: 4},  5000)
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
+// /*
+//   === Humanoid (Having an appearance or character resembling that of a human.) ===
+//   * team
+//   * weapons
+//   * language
+//   * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+//   * should inherit destroy() from GameObject through CharacterStats
+//   * should inherit takeDamage() from CharacterStats
+// */
 
-function Humanoid (team, weapons, language) {
-  GameObject.call (this, createdAt, name, dimensions);
-  CharacterStats.call(healthPoints);
+// function Humanoid (createdAt, name, dimensions, healthPoints, team, weapons, language) {
+//   GameObject.call (this, createdAt, name, dimensions);
+//   CharacterStats.call(healthPoints);
+//   this.createdAt = createdAt;
+//   this.name = name;
+//   this.dimensions = dimensions;
+//   this.healthPoints = healthPoints;
+//   this.team = team;
+//   this.weapons = weapons;
+//   this.language = language;
+// };
 
-  this.team = team;
-  this.weapons = weapons;
-  this.language = language;
-};
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-Humanoid.prototype.greet = function() {
-  return (`${name} offers a greeting in ${language}`)
-};
+// Humanoid.prototype = Object.create(CharacterStats.prototype);
+// Humanoid.prototype.greet = function() {
+//   return (`${this.name} offers a greeting in ${this.language}`)
+// };
+
+// const matt = new Humanoid("Yesterday", "Matt", {length: 3, width: 3, height: 3}, 1000000, "Home Team", "💪💪 These Guns", "JavaScript");
+
+// const matt2 = new Humanoid({
+//   createdAt: "Last Night",
+//   dimensions: {
+//     length: 2,
+//     width: 2,
+//     height: 5
+//   },
+//   healthPoints: 1000000,
+//   team: "Away Team",
+//   weapons: "🥾🥾 Boot in the ass",
+//   language: "Python",
+// });
+
+// console.log(matt)
+// console.log(matt2)
 
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -184,6 +211,106 @@ Humanoid.prototype.greet = function() {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
+
+  // const mage = new Humanoid({
+  //   createdAt: new Date(),
+  //   dimensions: {
+  //     length: 2,
+  //     width: 1,
+  //     height: 1,
+  //   },
+  //   healthPoints: 5,
+  //   name: 'Bruce',
+  //   team: 'Mage Guild',
+  //   weapons: [
+  //     'Staff of Shamalama',
+  //   ],
+  //   language: 'Common Tongue',
+  // });
+  // const swordsman = new Humanoid({
+  //   createdAt: new Date(),
+  //   dimensions: {
+  //     length: 2,
+  //     width: 2,
+  //     height: 2,
+  //   },
+  //   healthPoints: 15,
+  //   name: 'Sir Mustachio',
+  //   team: 'The Round Table',
+  //   weapons: [
+  //     'Giant Sword',
+  //     'Shield',
+  //   ],
+  //   language: 'Common Tongue',
+  // });
+  // const archer = new Humanoid({
+  //   createdAt: new Date(),
+  //   dimensions: {
+  //     length: 1,
+  //     width: 2,
+  //     height: 4,
+  //   },
+  //   healthPoints: 10,
+  //   name: 'Lilith',
+  //   team: 'Forest Kingdom',
+  //   weapons: [
+  //     'Bow',
+  //     'Dagger',
+  //   ],
+  //   language: 'Elvish',
+  // });
+  // console.log(mage.createdAt); // Today's date
+  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  // console.log(swordsman.healthPoints); // 15
+  // console.log(mage.name); // Bruce
+  // console.log(swordsman.team); // The Round Table
+  // console.log(mage.weapons); // Staff of Shamalama
+  // console.log(archer.language); // Elvish
+  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  // console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+
+
+
+
+
+/*
+TASK 5 🚀
+// convert the constructor functions above to class syntax copy and paste the objects and console logs below the class syntax to test if your code is working
+ */
+
+ class GameObject{
+   constructor(attrs){
+    this.createdAt = attrs.createdAt;
+    this.name = attrs.name;
+    this.dimensions = attrs.dimensions;
+   }
+   destroy() {
+     return `${this.name} was removed from the game.`}
+      };
+
+  class CharacterStats extends GameObject{
+    constructor(attrs){
+      super(attrs);
+      this.healthPoints = attrs.healthPoints;
+    }
+    takeDamage() {
+      return (`${name} took damage.`)
+    }
+  };
+  class Humanoid extends CharacterStats {
+    constructor(attrs){
+      super(attrs);
+      this.team = attrs.team;
+      this.weapons = attrs.weapons;
+      this.language = attrs.language;
+    }
+    greet() {
+      return (`${this.name} offers a greeting in ${this.language}`)
+    }
+  };
 
   const mage = new Humanoid({
     createdAt: new Date(),
@@ -242,14 +369,3 @@ Humanoid.prototype.greet = function() {
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
-
-
-
-
-
-
-/*
-TASK 5 🚀
-// convert the constructor functions above to class syntax copy and paste the objects and console logs below the class syntax to test if your code is working
- */
